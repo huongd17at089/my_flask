21731e3a0145b8976f2d2102ec1acc88d33bb347
dinhgihuong <huongdtt.b17at089@gmail.com>
2021-08-13 23:31:58 +0700

    init

--- app.py
+++ app.py
@@ -15,6 +15,10 @@ def user(name):
 
 @app.route("/run")
 def run():
+
+    return "<h1>model is available</h1>"
+
+if __name__ == "__main__":
     global sess
     sess = tf.compat.v1.Session()
     tf.compat.v1.keras.backend.set_session(sess)
@@ -22,8 +26,5 @@ def run():
     model = keras.models.load_model('checkpoint.h5', compile=False)
     global graph
     graph = tf.compat.v1.get_default_graph()
-    return "<h1>model is available</h1>"
-
-if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
